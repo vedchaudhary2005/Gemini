e0b7b480b4185fcd075db2b048a872fd285e0300
Ved Chaudhary <vedchaudhary162005@gmail.com>
2026-07-01 14:39:22 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js"
 
 const app=express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials:true
 }))
 const port=process.env.PORT || 5000
