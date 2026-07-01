e2cb48084cb82a8578113535e7053f9f618fda1d
Ved Chaudhary <vedchaudhary162005@gmail.com>
2026-07-01 15:30:32 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,17 @@ app.use(cors({
 const port=process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
+
+
+
 app.use("/api/auth",authRouter)
 app.use("/api/user",userRouter)
+app.get("/", (req, res) => {
+    res.send("Gemini Backend is running 🚀");
+});
+
+app.use("/api/auth", authRouter)
+app.use("/api/user", userRouter)
 
 
 connectDb()
